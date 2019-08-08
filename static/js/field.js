0d4592d419fe016d05e5de60cd2706ab4bd6a6a2
勾国印 <gouguoyin@juzifenqi.com>
2019-08-08 16:26:05 +0800

    优化字段录入体验

--- static/js/field.js
+++ static/js/field.js
@@ -1,1 +1,193 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('7 Q(U){2 W=$(\'#1d .11\');W.1Q({1P:U,1T:8,1S:1O,1K:I})}7 1J(r){2 l=["1a","r"];2 a=$(r);2 D=a.w(\'g\');2 K=D.6(\'.1L\');2 S=D.G("g");2 h=D.6("b.f").5();2 c=S.6("b.f").5();4(!h){h=0}4(!c){c=0}4($.21(a.5(),l)<0){K.s("m",e)}d{K.s("m",I)}4(h>=c){a.19(\'y\').s("m",I)}d{a.19(\'y\').22(\'y[A="1a"],y[A="r"]\').s("m",e)}}7 23(r,l){2 a=$(r);2 Z=a.w(\'.1V\').6(\'.z\');2 C=a.w(\'g\');2 q=1U(C.6(\'b.f\').5());4(l==\'1W\'){2 k=$(\'.p-z .1Y\').p(e)}d 4(l==\'1X\'){2 k=$(\'.p-z .1I\').p(e)}d 4(l==\'1q\'){2 k=$(\'.p-z .1n\').p(e)}4(q>=0){2 J=(q+1)*10+12}d{2 J=12}4(C.T>0){k.6("b.f").5(q+1).1t(\'q\',q+1);C.1o(k).G(\'g\').6(\'b.11\').1E(\'1D-1C\',J+\'1H\').N()}d{k.6("b.f").5(0);k.1F(Z).6(\'b:1x(0)\').N()}$(\'.1b\').1c("17");Q(["n","n-1B","n-1A","n-1z","n-25","n-2E","2z","2y-2A","2C","2B","v-2x","v-2t","v-2s","v-2u","2w","2v-2K","X-2J-2I","X-14-2L","X-14-2H","X-2d-2c"])}7 2e(13){2 a=$(13).w(\'g\');2 15=a.2g("g");2 Y=a.G("g");2 h=a.6("b.f").5();2 E=15.6("b.f").5();2 c=Y.6("b.f").5();4(!h){h=0}4(!E){E=0}4(!c){c=0}4(h>=c){a.29();$(\'.1b\').1c("17")}d{x(\'2r\');u}}7 F(R,18,O){2 P=2q 2m(18,"2h");u R.2j(P,O)}7 B(V){2 M=$(\'#\'+V).6(\'2l\').6(\'g\');4(M.T<=0){u\'\'}2 9="[";2 i=0;2 j=0;M.H(7(){i=i+1;j=0;4(i!=1){9+=","}9+="{";$(1l).6(\'1k\').6(\'b\').H(7(i,5){j=j+1;4(j!=1){9+=","}9+="\\""+5.1e+"\\":\\""+F(5.A,\'"\',\'\\\\"\')+"\\""});$(1l).6(\'1k\').6(\'2k\').H(7(i,5){j=j+1;4(j!=1){9+=","}9+="\\""+5.1e+"\\":\\""+F(5.A,\'"\',\'\\\\"\')+"\\""});9+="}"});9+="]";u 9}7 2i(){2p(\'2n，2o？\',7(){2a.1g.1h()})}7 28(t){$("1f").26({27:7(1j,o){4(!o.2b.2f("1f")){4(3==o.l){x(1j,\'16\')}}},L:"L",2F:t,2G:e,2D:e,1w:e,1y:7(){$(t).s("m","m");$(".1G").5(B(\'1d\'));$(".1p").5(B(\'1u\'));$(".1v").5(B(\'1s\'))},1r:7(9){4(9.1Z==\'1i\'){x(9.1m,\'1i\',7(){24.1g.1h()})}d{$(".20"+9.L).N().1M(\'1N\');$(t).1R("m");x(9.1m,\'16\')}}})}',62,172,'||var||if|val|find|function||json|thisObj|input|nextLevel|else|true|js_level|tr|thisLevel|||cloneObj|type|disabled|Accept||clone|level|object|attr|submitBtn|return|Content|closest|alert|option|table|value|getTableJson|TrObj|thisTrObj|preLevel|replaceAll|next|each|false|pl|addBtnObj|label|trObj|focus|newStr|regExp|selectHeader|originalStr|nextTrObj|length|headerParams|tableId|selectHeaderBtn||nextObj|tableObj||js_name||btn|Forwarded|preObj|error|change|oldStr|children|array|js_type|trigger|headerParamTable|name|form|location|reload|success|msg|td|this|message|js_responseClone|after|js_requestJson|response|callback|responseParamTable|data|requestParamTable|js_responseJson|ajaxPost|eq|beforeSubmit|Language|Encoding|Charset|left|padding|css|appendTo|js_headerJson|px|js_requestClone|selectType|autoSelect|js_addField|addClass|Validform_error|100|source|typeahead|removeAttr|delay|items|parseInt|row|header|request|js_headerClone|status|js_|inArray|not|addField|parent|Datetime|Validform|tiptype|submitForm|remove|window|obj|Token|Csrf|deleteField|is|prev|gm|cancelSave|replace|select|tbody|RegExp|您编辑的内容还没有保存|确定要退出吗|confirm|new|请先删除子参数|Type|Length|MD5|User|Referer|Disposition|Cache|Authorization|Control|Cookie|Connection|ignoreHidden|Ranges|btnSubmit|tipSweep|Host|With|Requested|Agent|For'.split('|'),0,{}))
+// 选择header
+function selectHeader(headerParams) {
+    var selectHeaderBtn = $('#headerParamTable .js_name');
+    selectHeaderBtn.typeahead({
+        source: headerParams,
+        items: 8, //显示8条
+        delay: 100, //延迟时间
+        autoSelect:false
+
+    });
+}
+// 选择类型
+function selectType(object) {
+    var type = ["array", "object"];
+
+    var thisObj = $(object);
+    var thisTrObj = thisObj.closest('tr');
+    var addBtnObj = thisTrObj.find('.js_addField');
+
+    var nextTrObj = thisTrObj.next("tr");
+
+    var thisLevel = thisTrObj.find("input.js_level").val();
+    var nextLevel = nextTrObj.find("input.js_level").val();
+
+    if(!thisLevel){
+        thisLevel = 0;
+    }
+
+    if(!nextLevel){
+        nextLevel = 0;
+    }
+
+    if($.inArray(thisObj.val(), type) < 0){
+        addBtnObj.attr("disabled", true);
+    }else{
+        addBtnObj.attr("disabled", false);
+    }
+
+    if(thisLevel >= nextLevel){
+        thisObj.children('option').attr("disabled",false);
+    }else{
+        thisObj.children('option').not('option[value="array"],option[value="object"]').attr("disabled",true);
+    }
+}
+
+// 获取最大id
+function getMaxId(tabel) {
+    var ids = [];
+    $(tabel).find(".js_id").each(function() {
+        ids.push($(this).val());
+    });
+
+    if(ids.length == 0){
+        return 0;
+    }
+
+    return parseInt(ids.sort().reverse()[0]);
+}
+
+// 新增字段
+function addField(object, type) {
+
+
+    var thisObj  = $(object);
+    var tableObj = thisObj.closest('.row').find('.table');
+    var TrObj    = thisObj.closest('tr');
+
+    var parentId = parseInt(thisObj.data('parent_id'));
+    var level    = parseInt(TrObj.find('input.js_level').val());
+    var width    = (10-(level+1))*10;
+
+    if(type == 'header'){
+        var cloneObj = $('.clone-table .js_headerClone').clone(true);
+        var id = getMaxId("#headerParamTable");
+    }else if(type == 'request'){
+        var cloneObj = $('.clone-table .js_requestClone').clone(true);
+        var id = getMaxId("#requestParamTable");
+    }else if(type == 'response'){
+        var cloneObj = $('.clone-table .js_responseClone').clone(true);
+        var id = getMaxId("#responseParamTable");
+    }
+
+    console.log(id);
+
+    if(id <= 0){
+        id = 1;
+    }
+
+    cloneObj.find('input.js_id').attr('value', id+1);
+    cloneObj.find('input.js_parent_id').attr('value', parentId);
+    cloneObj.find('.js_addField').attr('data-parent_id', id+1);
+
+    if(TrObj.length > 0){
+        cloneObj.find("input.js_level").attr('value', level + 1);
+        TrObj.after(cloneObj).next('tr').find('input.js_name').css('width', width + '%').focus();
+    }else{
+        cloneObj.find("input.js_level").attr('value', 0);
+        cloneObj.appendTo(tableObj).find('input:eq(0)').focus();
+    }
+
+    $('.js_type').trigger("change");
+
+    selectHeader(["Accept","Accept-Charset","Accept-Encoding","Accept-Language","Accept-Datetime","Accept-Ranges","Authorization","Cache-Control","Connection","Cookie","Content-Disposition","Content-Length","Content-Type","Content-MD5","Referer","User-Agent","X-Requested-With","X-Forwarded-For","X-Forwarded-Host","X-Csrf-Token"]);
+
+}
+
+// 删除字段
+function deleteField(btn) {
+    var thisObj = $(btn).closest('tr');
+    var preObj  = thisObj.prev("tr");
+    var nextObj = thisObj.next("tr");
+
+    var thisLevel = thisObj.find("input.js_level").val();
+    var preLevel  = preObj.find("input.js_level").val();
+    var nextLevel = nextObj.find("input.js_level").val();
+
+    if(!thisLevel){
+        thisLevel = 0;
+    }
+
+    if(!preLevel){
+        preLevel = 0;
+    }
+
+    if(!nextLevel){
+        nextLevel = 0;
+    }
+
+    // console.log(thisLevel);
+    // console.log(nextLevel);
+
+    if(thisLevel >= nextLevel){
+        thisObj.remove();
+        // 让非复合选项可选
+        $('.js_type').trigger("change");
+    }else{
+        alert('请先删除子参数');return;
+    }
+}
+
+function replaceAll(originalStr,oldStr,newStr){
+    var regExp = new RegExp(oldStr,"gm");
+    return originalStr.replace(regExp,newStr)
+}
+
+// 根据表格获取json字符串
+function getTableJson(tableId) {
+    
+    var trObj = $('#' + tableId).find('tbody').find('tr');
+
+    if(trObj.length <= 0){
+        return '';
+    }
+
+    var json = "[";
+    var i = 0;
+    var j = 0;
+
+    trObj.each(function() {
+        i = i + 1;
+        j = 0;
+        if (i != 1){
+            json += ",";
+        }
+
+        json += "{";
+        $(this).find('td').find('input').each(function(i, val) {
+            j = j + 1;
+            if (j != 1){
+                json += ",";
+            }
+            json += "\"" + val.name + "\":\"" + replaceAll(val.value,'"','\\"') + "\""
+        });
+        $(this).find('td').find('select').each(function(i, val) {
+            j = j + 1;
+            if (j != 1){
+                json += ",";
+            }
+
+            json += "\"" + val.name + "\":\"" + replaceAll(val.value,'"','\\"') + "\""
+        });
+        json += "}"
+    });
+    json += "]";
+    return json;
+}
+
+// 取消保存
+function cancelSave() {
+    confirm('您编辑的内容还没有保存，确定要退出吗？', function () {
+        window.location.reload();
+    });
+}
