0163630d04d516d58c14e6ed020ae733c61b0b65
勾国印 <gouguoyin@juzifenqi.com>
2019-08-09 14:05:00 +0800

    $register_email_suffix = '@' . explode('@', $this->email)[1]; 优化为 $register_email_suffix = stristr($this->email, "@")

--- models/account/RegisterForm.php
+++ models/account/RegisterForm.php
@@ -70,7 +70,7 @@ class RegisterForm extends Account
         $email_white_list = array_filter(explode("\r\n", trim($config->email_white_list)));
         $email_black_list = array_filter(explode("\r\n", trim($config->email_black_list)));
 
-        $register_email_suffix = '@' . explode('@', $this->email)[1];
+        $register_email_suffix = stristr($this->email,"@");
 
         if($email_white_list && !in_array($register_email_suffix, $email_white_list)){
             $this->addError($attribute, '该邮箱后缀不在可注册名单中');
