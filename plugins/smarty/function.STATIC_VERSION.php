9963210351dd287ecc10a5f14400a23a20f3f557
勾国印 <gouguoyin@juzifenqi.com>
2019-08-09 15:09:12 +0800

    静态资源版本号

--- plugins/smarty/function.STATIC_VERSION.php
+++ plugins/smarty/function.STATIC_VERSION.php
@@ -1,8 +1,11 @@
 <?php
-
+/**
+ * 静态资源版本号
+ * @param $params
+ * @param $smarty
+ * @return mixed
+ */
 function smarty_function_STATIC_VERSION($params, &$smarty)
 {
-
     return Yii::$app->params['static_version'];
-    
 }
