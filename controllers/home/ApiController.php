bc440fdb0944d2aea74f3cfcc0f9255a10e25f5d
勾国印 <gouguoyin@juzifenqi.com>
2019-08-21 17:18:12 +0800

    修复接口主页无法导出html文档的BUG

--- controllers/home/ApiController.php
+++ controllers/home/ApiController.php
@@ -1,12 +1,12 @@
 <?php
 namespace app\controllers\home;
 
-use app\models\Field;
 use Yii;
 use yii\helpers\Url;
 use yii\web\Response;
 use app\models\Module;
 use app\models\Api;
+use app\models\Field;
 use app\models\api\CreateApi;
 use app\models\api\UpdateApi;
 use app\models\api\DeleteApi;
@@ -168,7 +168,7 @@ class ApiController extends PublicController
      */
     public function actionExport($id)
     {
-        $api = Api::findModel(['encode_id' => $id]);
+        $api = Field::findModel(['encode_id' => $id]);
 
         if(!$api->project->hasAuth(['api' => 'export'])) {
             return $this->error('抱歉，您没有操作权限');
