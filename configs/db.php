1c7d0fe75780cdd3139da2628a7e412c879e276b
gouguoyin <245629560@qq.com>
2020-03-02 22:31:18 +0800

    encode_id字符串长度统一为50

--- configs/db.php
+++ configs/db.php
@@ -1,11 +1,6 @@
 <?php
 return
-array (
-  'class' => 'yii\\db\\Connection',
-  'dsn' => 'mysql:host=114.55.146.244;port=3306;dbname=phprap',
-  'username' => 'gouguoyin',
-  'password' => '68823095',
-  'charset' => 'utf8',
-  'tablePrefix' => 'doc_',
-)
+    array (
+        'class' => 'yii\\db\\Connection',
+    )
 ?>
