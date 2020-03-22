e5601b413c38e277248ff4f1acd4aa2688a5d35b
gouguoyin <245629560@qq.com>
2020-03-22 23:44:53 +0800

    Update db.php

--- configs/db.php
+++ configs/db.php
@@ -2,10 +2,5 @@
 return
 array (
   'class' => 'yii\\db\\Connection',
-  'dsn' => 'mysql:host=114.55.146.244;port=3306;dbname=phprap',
-  'username' => 'gouguoyin',
-  'password' => '68823095',
-  'charset' => 'utf8',
-  'tablePrefix' => 'doc_',
 )
 ?>
